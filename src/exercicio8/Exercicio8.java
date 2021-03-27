1b40e2863927e81a5d4a66007df866281639e7e2
isabela-coder <81197670+isabela-coder@users.noreply.github.com>
2021-03-27 00:17:08 -0300

    Aula 22-03 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/exercicio8/Exercicio8.java
@@ -0,0 +1,38 @@
+
+package exercicio8;
+
+import java.util.Scanner;
+
+public class Exercicio8 {
+    
+public static void main(String[] args) {
+        
+   Scanner teclado= new Scanner(System.in); 
+    double raio;
+     double diametro;
+     double area;
+         
+   System.out.println("Digite o valor do raio do circulo");
+    raio=teclado.nextDouble();
+    diametro= raio*2;
+    area=(3.1416)*(raio*raio);
+    
+     System.out.println("A area do circulo é de:"+area);
+         
+             
+        
+        
+        
+    }
+        
+    }
+       
+
+
+
+
+
+        
+        
+    
+    
